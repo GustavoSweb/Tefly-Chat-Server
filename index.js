c125cd2989ea10afe30887d2df95265f8781fd1b
Gustavo-Rodrigues <gustavodasilvama10@gmail.com>
2024-01-11 15:58:34 -0300

    Finished project

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app);
+const io = require('socket.io')(http,{cors: {
+    origin: 'http://localhost:8080',  // Permitir solicitações apenas do domínio http://localhost:8080
+    methods: ['GET', 'POST'],
+  }})
+var cors = require('cors')
 
-const io = require('socket.io')(http)
+app.use(cors())
 
-app.get('/', ()=>{})
+io.on('connection', (socket)=>{
+    console.log('teste')
+    socket.on('msg', (data)=>{
+        io.emit('newmsg', data)
+    })
+})
+app.get('/', (req, res)=>{
+    res.send('Ola')
+})
 
 http.listen(8081, ()=>{
     console.log('Servidor rodando')
